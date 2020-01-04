7ce3640a31227aff356e2ec1bfe79b425bea1ac7
Stephen Sloat <stephenlsloat@gmail.com>
2020-01-04 14:16:36 -0600

    update groups

--- app/forms.py
+++ app/forms.py
@@ -6,8 +6,8 @@ class GoogleGroups(FlaskForm):
     group = RadioField(
         'Which emails do you want:',
         choices = [
-            ('wheaton-ultimate', 'Ultimate frisbee games and social events'),
-            ('wheaton-ultimate-abridged', 'Ultimate frisbee games'),
+            ('wheaton-ultimate', 'Social events'),
+            ('wheaton-ultimate-frisbee', 'Ultimate frisbee games'),
             ('wheaton-soccer', 'Soccer games'),
             ('wheaton-housing', 'Housing roommates/tenants'),
         ],
