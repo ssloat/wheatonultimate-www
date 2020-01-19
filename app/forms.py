367a12beb055f5f644ca57bff6a1c562baa9baf9
Stephen Sloat <stephenlsloat@gmail.com>
2020-01-19 13:15:09 -0600

    update signup page - add subscribe/unsubscribe radio and use multi checkbox for groups

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,27 @@
 from flask_wtf import FlaskForm
 
-from wtforms import RadioField
+from wtforms import RadioField, SelectMultipleField, widgets
+
+class MultiCheckBoxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 class GoogleGroups(FlaskForm):
-    group = RadioField(
-        'Which emails do you want:',
+    action = RadioField(
+        'Are you signing up or leaving?',
+        choices = [
+            ('+subscribe', 'Subscribe'),
+            ('+unsubscribe', 'Unsubscribe'),
+        ],
+        default='+subscribe'
+    )
+
+    group = MultiCheckBoxField(
+        'Which emails are you signing up for or leaving?',
         choices = [
             ('wheaton-ultimate', 'Social events'),
             ('wheaton-ultimate-frisbee', 'Ultimate frisbee games'),
             ('wheaton-soccer', 'Soccer games'),
             ('wheaton-housing', 'Housing roommates/tenants'),
         ],
-        default = 'wheaton-ultimate',
     )
