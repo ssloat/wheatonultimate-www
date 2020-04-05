e10e89bcd07a42704e569e99760301efec2f6717
Stephen Sloat <stephenlsloat@gmail.com>
2020-04-05 13:22:53 -0500

    separate unsubscribe page.  different instructions for gmail/non-gmail accounts

--- app/forms.py
+++ app/forms.py
@@ -6,18 +6,20 @@ class MultiCheckBoxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
 
-class GoogleGroups(FlaskForm):
-    action = RadioField(
-        'Are you signing up or leaving?',
+class GoogleGroupsSubscribe(FlaskForm):
+    group = MultiCheckBoxField(
+        'Which emails are you signing up for?',
         choices = [
-            ('+subscribe', 'Subscribe'),
-            ('+unsubscribe', 'Unsubscribe'),
+            ('wheaton-ultimate', 'Social events'),
+            ('wheaton-ultimate-frisbee', 'Ultimate frisbee games'),
+            ('wheaton-soccer', 'Soccer games'),
+            ('wheaton-housing', 'Housing roommates/tenants'),
         ],
-        default='+subscribe'
     )
 
+class GoogleGroupsUnsubscribe(FlaskForm):
     group = MultiCheckBoxField(
-        'Which emails are you signing up for or leaving?',
+        'Which emails are you unsubscribing from?',
         choices = [
             ('wheaton-ultimate', 'Social events'),
             ('wheaton-ultimate-frisbee', 'Ultimate frisbee games'),
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask import render_template
-from app import app
+import os
 
-from app.forms import GoogleGroups
+from flask import render_template
+from app import app, forms
+
+from pymongo import MongoClient
 
 @app.route('/')
 @app.route('/index')
@@ -10,6 +12,48 @@ def index():
 
 @app.route('/signup')
 def signup():
-    groups = GoogleGroups()
+    groups = forms.GoogleGroupsSubscribe()
     return render_template('signup.html', title='Email lists', form=groups)
+
+@app.route('/unsubscribe')
+def unsubscribe():
+    groups = forms.GoogleGroupsUnsubscribe()
+    return render_template('unsubscribe.html', title='Email lists', form=groups)
+
+"""
+@app.route('/verify')
+def verify():
+    groups = GoogleGroups()
+    return render_template('verify.html', title='Email lists', form=groups)
+
+@app.route('/ultimate')
+def ultimate():
+    threads = get_threads('wheaton-ultimate-frisbee')
+    return render_template('topics.html', title='Ultimate', threads=threads)
+
+@app.route('/housing')
+def housing():
+    threads = get_threads('wheaton-housing')
+    return render_template('topics.html', title='Housing', threads=threads)
+
+@app.route('/soccer')
+def soccer():
+    threads = get_threads('wheaton-soccer')
+    return render_template('topics.html', title='Soccer', threads=threads)
+
+@app.route('/social')
+def social():
+    threads = get_threads('wheaton-ultimate')
+    return render_template('topics.html', title='Social', threads=threads)
+
+
+@app.route('/google')
+def google():
+    return render_template('google.html')
+
+
+def get_threads(group):
+    mongo = MongoClient(os.environ.get('MONGO_DB_URI'))['slack']
+    return mongo.threads.find({'group': group})
+"""
 
